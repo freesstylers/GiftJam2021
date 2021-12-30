8dd9d152c7fd88f495c7a8223e19ffda44785ed9
Posna <jomart17@ucm.es>
2021-12-30 20:15:16 +0100

    Movimiento Eurico

--- Regalito-Mermelada/Assets/Scripts/Player/PlayerMovement.cs
+++ Regalito-Mermelada/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,26 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement behaviour")]
     public float speed = 1f;
-    Transform tr_;
-    private void Start()
+    public CharacterController cController;
+    private Transform playerRot;
+
+    [Header("Camera behaviour")]
+    public float rotSpeed;
+
+    void Start()
     {
-        tr_ = GetComponent<Transform>();
+        playerRot = gameObject.GetComponent<Transform>();
     }
 
     void Update()
     {
+        HandleMovement();
+        HandleCamera();
+    }
+
+    void HandleMovement()
+    {
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
 
-        Vector3 dir = new Vector3(hor, 0f, ver).normalized;
+        Vector3 dir = new Vector3(-hor, 0f, -ver);
+        dir = playerRot.TransformDirection(dir);
+        cController.Move(dir * speed * Time.deltaTime);
+    }
 
-        if (dir.magnitude > 0.1f)
-        {
-            tr_.position += dir * speed * Time.deltaTime;
-        }
+    void HandleCamera()
+    {
+        float l = Convert.ToInt32(Input.GetKey(KeyCode.Q));
+        float r = Convert.ToInt32(Input.GetKey(KeyCode.E));
+
+        playerRot.Rotate(Vector3.up, (r - l) * rotSpeed);
     }
 }
